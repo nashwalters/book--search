9d677346a970e525ec6b8bc87d81d2ec39801243
Nash <72932477+Nashwalt@users.noreply.github.com>
2021-04-24 19:59:02 -0400

    Added Models with book schema.

---
+++ models/book.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+ 
+let bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    authors: {
+        type: [{ type: String, required: true }],
+        required: true
+    },
+    description: {
+        type: String
+    },
+    image: {
+        type: String
+    },
+    link: {
+        type: String
+    }
+})
+
+const Book = mongoose.model("Book", bookSchema)
+
+module.exports = Book
