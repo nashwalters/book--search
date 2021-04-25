b1dbbde340268bbc6e31e0dfcd5d93e78f5f4241
Nash <72932477+Nashwalt@users.noreply.github.com>
2021-04-24 20:12:47 -0400

    Mods made to server and database coonected.

--- server.js
+++ server.js
@@ -2,16 +2,31 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require('mongoose');
+const routes = require('./routes');
 
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+app.use(routes)
+
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  
 });
 
 app.listen(PORT, function() {
