4fc1ffbd8284bf17004fd4031a234d0a3f997a60
Nash <72932477+Nashwalt@users.noreply.github.com>
2021-04-27 13:02:25 -0400

    Deployed.

---
+++ client/build/precache-manifest.b4865027578b324185b5d56dec4ccd75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "960dd6d00325d0524012",
+    "url": "/static/css/main.8612891a.chunk.css"
+  },
+  {
+    "revision": "960dd6d00325d0524012",
+    "url": "/static/js/main.c8a33d5e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f31dd3935c9323a0520c",
+    "url": "/static/js/2.e724740b.chunk.js"
+  },
+  {
+    "revision": "827d8eff9a0360b5c68430f212e89f29",
+    "url": "/index.html"
+  }
+];
