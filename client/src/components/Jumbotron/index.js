c2bd6389f951b72137b98ec2111455ae7add82e4
Nash <72932477+Nashwalt@users.noreply.github.com>
2021-04-27 14:26:28 -0400

    Deployed.

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Jumbotron = () => {
   return (
-    <div class="jumbotron mt4">
-      <div class="container">
-        <h1 class="display-4">Google Books Search</h1>
-        <h5 class="lead">(Search and Save Books of Interest)</h5>
+    <div className="jumbotron mt4">
+      <div className="container">
+        <h1 className="display-4">Google Books Search</h1>
+        <h5 className="lead">(Search and Save Books of Interest)</h5>
       </div>
     </div>
   );
